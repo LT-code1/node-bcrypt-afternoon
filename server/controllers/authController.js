189c0c3632be97cf1b0214cd5656584857fef7c0
wade wilson <LayneThompson@gmail.com>
2019-08-15 12:06:23 -0500

    second

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,33 +1,81 @@
-
 const bcrypt = require("bcryptjs");
 
-function register(req,res) {
-    // console.log(req);
-    const {username, password, isAdmin} = req.body
+module.exports = {
+  register: async (req, res) => {
+    const { username, password, isAdmin } = req.body;
     const db = req.app.get("db");
+    const result = await db.get_user([username]);
+    const existingUser = result[0];
+    if (existingUser) {
+      return res.status(409).send("Username taken");
+    }
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    const registeredUser = await db.register_user([isAdmin, username, hash]);
+    const user = registeredUser[0];
+    req.session.user = {
+      isAdmin: user.is_admin,
+      username: user.username,
+      id: user.id
+    };
+    return res.status(201).send(req.session.user);
+  },
 
-    db.get_user(username)
-        .then(async result => {
-           // console.log(result[0]);
-            //console.log(isAdmin);
-            let existingUser = result[0];
+  login: async (req, res) => {
+    const { username, password } = req.body;
+    const foundUser = await req.app.get("db").get_user([username]);
+    const user = foundUser[0];
+    if (!user) {
+      return res
+        .status(401)
+        .send(
+          "User  not found. Please register as a new user before logging in."
+        );
+    }
+    const isAuthenticated = bcrypt.compareSync(password, user.hash);
+    if (!isAuthenticated) {
+      return res.status(403).send("Incorrect password");
+    }
+    req.session.user = {
+      isAdmin: user.is_admin,
+      id: user.id,
+      username: user.username
+    };
+    return res.send(req.session.user);
+  },
+  logout: (req, res) => {
+    req.session.destroy();
+    return res.sendStatus(200);
+  }
+};
 
-            if(result[0].count == 0){
-            // const salt = bcrypt.genSaltSync(10);
-            const hash = await bcrypt.hash(password, 10);
-            let registeredUser = [];
-            registeredUser = await db.register_user(isAdmin, username, hash);
-            let user = registeredUser[0];
-            req.session.user = {isAdmin:user.is_admin, id:user.id, username:user.username}
-            //console.log(req.session.user);
-            res.status(201).json(req.session.user);
-            }else {
-                res.status(409).json("username taken. choose another");
-            }
-    })
-}
+// function register(req,res) {
+//     // console.log(req);
+//     const {username, password, isAdmin} = req.body
+//     const db = req.app.get("db");
 
+//     db.get_user(username)
+//         .then(async result => {
+//            // console.log(result[0]);
+//             //console.log(isAdmin);
+//             let existingUser = result[0];
 
-module.exports={
-    register
-}
+//             if(result[0].count == 0){
+//             // const salt = bcrypt.genSaltSync(10);
+//             const hash = await bcrypt.hash(password, 10);
+//             let registeredUser = [];
+//             registeredUser = await db.register_user(isAdmin, username, hash);
+//             let user = registeredUser[0];
+//             req.session.user = {isAdmin:user.is_admin, id:user.id, username:user.username}
+//             //console.log(req.session.user);
+//             res.status(201).json(req.session.user);
+//             }else {
+//                 res.status(409).json("username taken. choose another");
+//             }
+//     })
+// }
+
+// module.exports={
+//     register,
+//     login
+// }
--- server/index.js
+++ server/index.js
@@ -1,30 +1,51 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const session = require('express-session');
-const massive = require('massive');
-const bcrypt = require("bcryptjs");
+const express = require("express");
+const session = require("express-session");
+const massive = require("massive");
+//const bcrypt = require("bcryptjs");
 
-const authCtrl = require('./controllers/authController');
+const authCtrl = require("./controllers/authController");
+const treasureCtrl = require("./controllers/treasureController");
+const auth = require("./middleware/authMiddleware");
 
-const app = express()
+const app = express();
 
 app.use(express.json());
 
 massive(process.env.CONNECTION_STRING)
-.then(dbinstance => {app.set('db',dbinstance);
-console.log("database connected :)");
-}).catch(e => console.log(e)); 
+  .then(dbinstance => {
+    app.set("db", dbinstance);
+    console.log("database connected :)");
+  })
+  .catch(e => console.log(e));
 
-app.use(session({
+app.use(
+  session({
     saveUninitialized: true,
     resave: false,
     secret: process.env.SESSION_SECRET,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 2
+      maxAge: 1000 * 60 * 60 * 24 * 2
     }
-}))
+  })
+);
 
-app.post('/api/register', authCtrl.register);
+//app.post("/api/register", authCtrl.register);
+app.post("/auth/register", authCtrl.register);
+app.post("/auth/login", authCtrl.login);
+app.get("/auth/logout", authCtrl.logout);
 
-app.listen(process.env.SERVER_PORT, () => console.log('Listening on Port ' + process.env.SERVER_PORT)) //runs the server on the given port
+app.get("/api/treasure/dragon", treasureCtrl.dragonTreasure);
+app.get("/api/treasure/user", auth.usersOnly, treasureCtrl.getUserTreasure);
+app.post("/api/treasure/user", auth.usersOnly, treasureCtrl.addUserTreasure);
+app.get(
+  "/api/treasure/all",
+  auth.usersOnly,
+  auth.adminsOnly,
+  treasureCtrl.getAllTreasure
+);
+
+app.listen(process.env.SERVER_PORT, () =>
+  console.log("Listening on Port " + process.env.SERVER_PORT)
+); //runs the server on the given port
